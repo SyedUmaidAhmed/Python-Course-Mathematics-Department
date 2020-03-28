ee850eac2da418f3a6cadae4f4a1275a7ce91a7e
Raspberry_Pi Pakistan <36759636+SyedUmaidAhmed@users.noreply.github.com>
2020-03-29 03:31:31 +0500

    Delete Slide_2_Find_maximum.py

--- Week 1 Installation and Introductionof Python/Slide_2_Find_maximum.py
+++
@@ -1,6 +0,0 @@
-x = float(input("Enter First Number: "))
-y = float(input("Enter Second Number: "))
-z = float(input("Enter Third Number: "))
-
-print("The maximum value is: ", max(x,y,z))
-          
